02eb9217afbbff2bc4f244f426316a300307eda4
161250116 <161250116@mail.smail.nju.edu.cn>
2020-01-18 21:50:13 +0800

    First Commit

---
+++ src/main/java/nju/edu/uml/webumldesigner/WebUmlDesignerApplication.java
@@ -0,0 +1,13 @@
+package nju.edu.uml.webumldesigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUmlDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebUmlDesignerApplication.class, args);
+	}
+
+}
