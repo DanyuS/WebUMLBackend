42b50b70ec5f1a5f2498078e93ffca340a2d85c9
161250127 <361487214@qq.com>
2020-03-04 17:37:56 +0800

    修改原因：修改增加结点 修改人：161250127

---
+++ src/main/java/nju/edu/uml/webumldesigner/controller/params/Style.java
@@ -0,0 +1,45 @@
+package nju.edu.uml.webumldesigner.controller.params;
+
+/**
+ * @Author: 161250127 TJW
+ * @Description:
+ * @Date: 2020/3/4
+ */
+public class Style {
+    private Integer width;
+    private Integer height;
+    private Integer left;
+    private Integer top;
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getLeft() {
+        return left;
+    }
+
+    public void setLeft(Integer left) {
+        this.left = left;
+    }
+
+    public Integer getTop() {
+        return top;
+    }
+
+    public void setTop(Integer top) {
+        this.top = top;
+    }
+}
