44cc1ef9c6f475200d312c1597f71ffef4869e32
161250116 <161250116@mail.smail.nju.edu.cn>
2020-03-10 23:37:13 +0800

    修改添加线条

--- src/main/java/nju/edu/uml/webumldesigner/controller/UserEditController.java
+++ src/main/java/nju/edu/uml/webumldesigner/controller/UserEditController.java
@@ -74,7 +74,7 @@ public class UserEditController {
         return editService.delNode(fid, nid);
     }
 
-    @GetMapping("/addLine")
+    @PostMapping("/addLine")
     public boolean addLine(@RequestBody LineParams lineParams) {
         return editService.addLine(lineParams);
 //        return editService.addLine(lineParams.getLineId(), lineParams.getRelationType(), lineParams.getFromId(), lineParams.getToId(), lineParams.getText(), lineParams.getMarkerStart(), line.getMarkerEnd(),linePositionList , startPosition, endPosition, lineStyle, lineSvgStyle, lineParams.getUid(), lineParams.getGid());
